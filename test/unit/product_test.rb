d7f7f32e813902c411fee6301dfde031d30daf67
Sjones352 <sjones@tistatech.com>
2018-05-19 16:13:46 -0400

    Validation!

--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -1,6 +1,8 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
+  fixtures :products
+
   test "product attributes must not be empty" do
   	product = Product.new
   	assert product.invalid?
@@ -21,15 +23,30 @@ test "product is not valid without a unique title" do
   	assert_equal "has already been taken", product.errors[:title].join(';')
  end
 
+test "product price must be positive" do
+     product = Product.new(title: "My Book Title",
+     description: "yyy",
+     image_url: "zzz.jpg")
+     product.price = -1
+     assert product.invalid?
+     assert_equal "must be greater than or equal to 0.01",
+     product.errors[:price].join('; ')
+     product.price = 0
+     assert product.invalid?
+     assert_equal "must be greater than or equal to 0.01",
+     product.errors[:price].join('; ')
+     product.price = 1
+     assert product.valid?
+end
 
 # test "product is not valid without a unique title -i18n" do
-#   	product = Product.new(
+#     product = Product.new(
 #         :title => products(:ruby).title,
 #         :description => "yyyy",
 #         :price => 1,
 #         :image_url => "fred.gif")
-#   	assert !product.save
-#   	assert_equal I18n.translate('activerecord.errors.messages.taken'),
-#   	product.errors[:title].join(';')
+#     assert !product.save
+#     assert_equal I18n.translate('activerecord.errors.messages.taken'),
+#     product.errors[:title].join(';')
 #   end
 end
