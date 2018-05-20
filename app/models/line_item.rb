d3012fbf1eef15119368e55bf67fd19d83ba3478
Sjones352 <sjones@tistatech.com>
2018-05-19 20:57:59 -0400

    added a smart cart

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -5,7 +5,6 @@ class LineItem < ActiveRecord::Base
 	belongs_to :cart
 
 	def total_price
-     product.price.to_s.to_d * quantity.to_s.to_d
-     # product.price * quantity
+     product.price * quantity
     end
 end
